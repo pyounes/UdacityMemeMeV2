5251c47287c62bfc90026b6f59039747ce6e0af7
Pierre Younes <pyounes@gmail.com>
2021-01-31 21:27:41 +0200

    Added MemeDetailViewController

---
+++ MemeMeV2/Controllers/MemeDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMeV2
+//
+//  Created by Pierre Younes on 1/31/21.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var memedImage: UIImageView!
+    
+    // Variables
+    var meme: Meme!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
